683a2a92dcab2b026ceed4697b94808198b134e9
subbareddya <subba9678@gmail.com>
2022-05-05 11:47:25 +0530

    hello world

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import FooterComponent from "./components/FooterComponent";
-import HeaderComponent from "./components/HeaderComponent";
-import JobInformation from "./components/JobInformation";
-import PersonalInformationComponent from "./components/PersonalInformationComponent";
-import EmergencyContactInformationComponent from './components/EmergencyContactInformationComponent';
-import SaveComponent from './components/SaveEmployeeComponent';
-import SaveEmployeeComponent from './components/SaveEmployeeComponent';
-import CreateEmployee from './components/CreateEmployee';
 
 function App() {
   return (
-    <div >
-      <CreateEmployee />
-      
-      <div>
-        <FooterComponent />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my React App.
+        </p>
+      </header>
     </div>
   );
 }
