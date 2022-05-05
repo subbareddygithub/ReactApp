916a5ce3cb1b57f31a69b1597188699c0521acc2
subbareddya <subba9678@gmail.com>
2022-05-05 11:09:38 +0530

    imports added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import JobInformation from "./components/JobInformation";
+import PersonalInformationComponent from "./components/PersonalInformationComponent";
+import EmergencyContactInformationComponent from './components/EmergencyContactInformationComponent';
+import SaveComponent from './components/SaveEmployeeComponent';
+import SaveEmployeeComponent from './components/SaveEmployeeComponent';
+import CreateEmployee from './components/CreateEmployee';
 
 function App() {
   return (
